c3cebdb95719338cf7f07ca40c62b7a5a34dff7b
Mani <manisreekar.sr@gmail.com>
2019-03-28 19:14:39 +0530

    first commit

---
+++ precache-manifest.634ed881ba14fb14c79b8b45c1473f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f30c80010ff66e384951",
+    "url": "/static/js/main.8a0141b7.chunk.js"
+  },
+  {
+    "revision": "093e8c666906de63a485",
+    "url": "/static/js/2.f180b32f.chunk.js"
+  },
+  {
+    "revision": "f30c80010ff66e384951",
+    "url": "/static/css/main.533ca068.chunk.css"
+  },
+  {
+    "revision": "093e8c666906de63a485",
+    "url": "/static/css/2.666544c1.chunk.css"
+  },
+  {
+    "revision": "f99619e88fcc02bac6706b149f77d8f1",
+    "url": "/index.html"
+  }
+];
